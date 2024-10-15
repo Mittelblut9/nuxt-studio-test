0ab73e36bf40bb5ded6f5d91c8b72892b40be997
Mittelblut9 <sauer.benedikt@outlook.de>
2024-10-15 20:12:40 +0200

    feat(app): add first custom component

---
+++ components/content/HomeFeature.component.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex items-center gap-3">
+    <div class="flex items-center justify-center border rounded-lg p-1.5">
+      <UIcon :name="icon" />
+    </div>
+    <div class="flex flex-col">
+      <h3 class="font-semibold">
+        <ContentSlot name="title" />
+      </h3>
+      <span>
+        <ContentSlot name="description" />
+      </span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'HomeFeatureComponent',
+  props: {
+    icon: {
+      type: String,
+      default: 'i-ph-cursor-click',
+    },
+  },
+}
+</script>
